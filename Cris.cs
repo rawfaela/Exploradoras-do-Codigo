c3e7289e158cc9a9581caafa9a68d860ef16bbae
rafa <168464894+rawfaela@users.noreply.github.com>
2024-11-04 15:16:05 -0300

    Update Cris.cs

--- Cris.cs
+++ Cris.cs
@@ -11,6 +11,8 @@ public class Cris : MonoBehaviour
     public bool isground;
     private bool hasJumped = false;
 
+    public Transform mari;
+
     void Start()
     {
         rigd = GetComponent<Rigidbody2D>();
@@ -62,22 +64,22 @@ public class Cris : MonoBehaviour
 
     void Jump()
     {
-        // Checa se est· no ch„o e se a tecla de pulo foi pressionada
+        // Checa se est√° no ch√£o e se a tecla de pulo foi pressionada
         if (isground && Input.GetButtonDown("Jump2") && !hasJumped)
         {
             rigd.velocity = new Vector2(rigd.velocity.x, jumpForce);
             isground = false;
             hasJumped = true;
-            anim.SetInteger("transitions", 2); // Define a animaÁ„o de salto
+            anim.SetInteger("transitions", 2); // Define a anima√ß√£o de salto
         }
     }
 
     void Fall()
     {
-        // Checa se o personagem est· caindo
+        // Checa se o personagem est√° caindo
         if (rigd.velocity.y < 0 && !isground)
         {
-            anim.SetInteger("transitions", 3); // Define a animaÁ„o de queda
+            anim.SetInteger("transitions", 3); // Define a anima√ß√£o de queda
         }
     }
 
@@ -92,9 +94,12 @@ public class Cris : MonoBehaviour
 
     void Death()
     {
-        Debug.Log("inimigp");
+        Debug.Log("inimigo");
         anim.SetTrigger("die");
         Destroy(gameObject, 0.8f);
+        
+        if (mari==null){
+            SceneManager.LoadScene("GameOver");
     }
 
 }
