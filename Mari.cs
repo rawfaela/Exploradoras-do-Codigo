c16a09070a7c8526d444be189ca4759b89459333
rafa <168464894+rawfaela@users.noreply.github.com>
2024-11-21 20:53:13 -0300

    Update Mari.cs

--- Mari.cs
+++ Mari.cs
@@ -67,22 +67,22 @@ public class Mari : MonoBehaviour
 
     void Jump()
     {
-        // Checa se est· no ch„o e se a tecla de pulo foi pressionada
+        // Checa se est√° no ch√£o e se a tecla de pulo foi pressionada
         if (isground && Input.GetButtonDown("Jump") && !hasJumped)
         {
             rigd.velocity = new Vector2(rigd.velocity.x, jumpForce);
             isground = false;
             hasJumped = true;
-            anim.SetInteger("transitions", 2); // Define a animaÁ„o de salto
+            anim.SetInteger("transitions", 2); // Define a anima√ß√£o de salto
         }
     }
 
     void Fall()
     {
-        // Checa se o personagem est· caindo
+        // Checa se o personagem est√° caindo
         if (rigd.velocity.y < 0 && !isground)
         {
-            anim.SetInteger("transitions", 3); // Define a animaÁ„o de queda
+            anim.SetInteger("transitions", 2); // Define a anima√ß√£o de queda
         }
     }
 
@@ -95,7 +95,7 @@ public class Mari : MonoBehaviour
             if (normal.x != 0)
             {
                 Debug.Log("!");
-                anim.SetTrigger("die");  //sÛ funciona quando quer
+                anim.SetTrigger("die");  //s√≥ funciona quando quer
                 Death();
             }
         
